694635feda36953c4ab81b42842c5b3bee91d348
Br <br@Brs-MacBook-Pro.local>
2019-03-23 23:41:15 -0700

    Testing checkpoint complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -47,4 +47,8 @@ module.exports = class MenuController {
         console.log("Thanks for using AddressBloc!");
         process.exit();
     }
+
+    getContactCount() {
+        return this.contacts.length;
+    }
 }
