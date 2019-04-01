29edf7098dc168fb5baf9b08f613e8a5ff894586
Br <br@Brs-MacBook-Pro.local>
2019-03-31 19:13:21 -0700

    modified spec menu_controller_spec.js

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -7,25 +7,4 @@ describe("MenuController", () => {
 
     });
 
-
-<<<<<<< HEAD
-        it("should return 0 when no contacts are in the book", () => {
-            expect(this.menu.getContactCount()).toBe(0);
-        });
-
-        it("should return 1 when there is exactly one contact in the book", () => {
-            this.menu.contacts.push("Bob");
-            expect(this.menu.getContactCount()).toBe(1);
-        });
-    });
-
-    describe("#remindMe()", () => {
-        it("should return a string contain the text", () => {
-            expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
-        });
-
-    })
-
-=======
->>>>>>> checkpoint-contact-connect
 });
